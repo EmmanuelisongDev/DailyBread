8b9d901f822255f0f80d46718a4804aa6b18c5f1
Emmanuel Isong <emmisongdev@gmail.com>
2024-01-19 23:35:16 +0100

    Resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-
 import Recipe from "./pages/Recipe";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar/Navbar";
